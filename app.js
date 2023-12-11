955e200af2f77d7c441114eebd319b790a152193
maliawanderer <malia.wanderer@gmail.com>
2023-12-10 21:54:35 -0500

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
-import PlayerRoutes from "./Players/routes"
+import playerRoutes from "./Players/routes.js"
 import UserRoutes from "./Users/routes"
 
 const CONNECTION_STRING = "mongodb"
@@ -11,6 +11,6 @@ const db = mongoose.connection;
 console.log(db);
 app.use(cors());
 app.use(express.json());
-PlayerRoutes(app);
+playerRoutes(app);
 UserRoutes(app);
 app.listen(process.env.PORT || 4000);
