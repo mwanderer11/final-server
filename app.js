a4f2f26181548cb067b2e21a3911843b0e141188
maliawanderer <malia.wanderer@gmail.com>
2023-12-10 21:48:39 -0500

    dotenv

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import UserRoutes from "./Users/routes"
 const CONNECTION_STRING = "mongodb"
 mongoose.connect(CONNECTION_STRING);const app = express();
 const db = mongoose.connection;
+console.log(db);
 app.use(cors());
 app.use(express.json());
 PlayerRoutes(app);
