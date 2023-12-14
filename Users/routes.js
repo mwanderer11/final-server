7fd30adfb62e5db3fc9d1dc81ef1dfe7b262eec8
maliawanderer <malia.wanderer@gmail.com>
2023-12-14 12:44:02 -0500

    test

--- Users/routes.js
+++ Users/routes.js
@@ -32,6 +32,7 @@ function UserRoutes(app) {
     const signin = async (req, res) => {
         const { username, password } = req.body;
         const currentUser = await dao.findUserByCredentials(username, password);
+        req.session['currentUser'] = currentUser;
         res.json(currentUser);
     };
 
